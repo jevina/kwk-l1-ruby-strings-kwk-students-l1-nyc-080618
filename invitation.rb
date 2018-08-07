2e60d1eabd959ab1d663f01b87d96dece0628103
jevina <jwong00@stuy.edu>
2018-08-07 15:56:29 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,7 @@
 # Code your prompts her
 def invitation(name)
-  puts "Dear #{name}
+  puts "Hi, you've been invited to a party! What is your name?
+  Dear #{name}
   You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
  
 Sincerely,
